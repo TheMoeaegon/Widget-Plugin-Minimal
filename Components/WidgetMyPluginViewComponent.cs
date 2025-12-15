460fd1c7099357834c132091bfbcb4305bad12cd
Moee1149 <shahi.kshitij55@gmail.com>
2025-12-15 23:09:39 +0545

    basic skeleton of plugin

---
+++ Components/WidgetMyPluginViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Widgets.MyPlugin.Components;
+
+public class WidgetMyPluginViewComponent : NopViewComponent
+{
+
+    #region Ctor
+    public WidgetMyPluginViewComponent() { }
+
+    #endregion
+
+    #region Methods
+
+    /// <returns>A task that represents the asynchronous operation</returns>
+    public async Task<IViewComponentResult> InvokeAsync(string widgetZone, object additionalData)
+    {
+        return View("~/Plugins/Widgets.MyPlugin/Views/Index.cshtml");
+    }
+
+    #endregion
+}
---
+++ MyPlugin.cs
@@ -0,0 +1,31 @@
+﻿using Nop.Plugin.Widgets.MyPlugin.Components;
+using Nop.Services.Cms;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+
+namespace Nop.Plugin.Widgets.MyPlugin;
+
+public class MyPlugin : BasePlugin, IWidgetPlugin
+{
+    public bool HideInWidgetList => throw new NotImplementedException();
+
+    public Type GetWidgetViewComponent(string widgetZone)
+    {
+        return typeof(WidgetMyPluginViewComponent);
+    }
+
+    public Task<IList<string>> GetWidgetZonesAsync()
+    {
+        return Task.FromResult<IList<string>>(new List<string> { PublicWidgetZones.HomepageTop, PublicWidgetZones.HomepageBeforeNews });
+    }
+
+    public override async Task InstallAsync()
+    {
+        await base.InstallAsync();
+    }
+
+    public override async Task UninstallAsync()
+    {
+        await base.UninstallAsync();
+    }
+}
